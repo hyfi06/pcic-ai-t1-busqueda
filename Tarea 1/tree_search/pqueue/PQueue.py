b866f64093bf020f071633584ba5f91be45b2265
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2023-08-26 08:35:13 -0600

    feat: add PQueue module
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

--- Tarea 1/tree_search/pqueue/PQueue.py
+++ Tarea 1/tree_search/pqueue/PQueue.py
@@ -1,17 +1,22 @@
-from typing import Any, Callable
+from typing import TypeAlias, Callable, Generic, TypeVar
+
+T = TypeVar('T')
+
+PriorityItem: TypeAlias = tuple[T, int]
+Strategy: TypeAlias = Callable[[list[PriorityItem]], PriorityItem]
 
 
-class PQueue:
-    queue_list: list[tuple[int, Any]] = list()
-    strategy: Callable[[list[tuple[int, Any]]], tuple[int, Any]]
+class PQueue(Generic[T]):
+    queue_list: list[PriorityItem] = list()
+    strategy: Strategy
 
-    def __init__(self, pop_strategy: Callable[[list[tuple[int, Any]]], tuple[int, Any]]) -> None:
+    def __init__(self, pop_strategy: Strategy) -> None:
         self.strategy = pop_strategy
 
-    def push(self, item: tuple[int, Any]) -> None:
+    def push(self, item: PriorityItem) -> None:
         self.queue_list.append(item)
 
-    def pop(self) -> tuple[int, Any]:
-        value: tuple[int, Any] = self.strategy(self.queue_list)
+    def pop(self) -> PriorityItem:
+        value: PriorityItem = self.strategy(self.queue_list)
         self.queue_list.remove(value)
         return value
