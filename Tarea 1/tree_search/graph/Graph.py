94c0b9c8422ac5b263316fc9a7b30ad549c1a75c
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2023-08-26 08:47:30 -0600

    feat: add LabeledGraph
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

--- Tarea 1/tree_search/graph/Graph.py
+++ Tarea 1/tree_search/graph/Graph.py
@@ -1,12 +1,16 @@
-from typing import NewType, TypeVar, Generic
+from typing import TypeAlias, TypeVar, Generic, Optional
 
-T = TypeVar('T')
-AdjacencyItem = NewType("AdjacencyList", tuple[T, int])
-AdjacencyTable = NewType("AdjacencyTable", dict[T, list[AdjacencyItem]])
+T = TypeVar('T', str, int)
+AdjacencyItem: TypeAlias = tuple[T, int]
+AdjacenciesTable: TypeAlias = dict[T, list[AdjacencyItem]]
 
 
-class Graph(Generic[T]):
-    adjacency_table: AdjacencyTable = dict()
+class LabeledGraph(Generic[T]):
+    adjacencies_table: AdjacenciesTable = dict()
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, adjacencies_table: Optional[AdjacenciesTable]) -> None:
+        if adjacencies_table:
+            self.adjacencies_table = adjacencies_table
+
+    def add_node(self, node_label: T, adjacencies: list[AdjacencyItem]):
+        self.adjacencies_table[node_label] = adjacencies
