47abc4e41d30d1958b3fd4fb9ea6c3e483319afc
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2023-08-27 15:50:40 -0600

    refactor: update LabeledGraph model
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

--- Tarea 1/tree_search/graph/models.py
+++ Tarea 1/tree_search/graph/models.py
@@ -5,21 +5,11 @@ AdjacenciesTable: TypeAlias = dict[str, list[AdjacencyItem]]
 
 
 class LabeledGraph:
-    adjacencies_table: AdjacenciesTable = dict()
-    _pointer: str = ''
-
     def __init__(self, adjacencies_table: Optional[AdjacenciesTable] = None) -> None:
-        if adjacencies_table:
-            self.adjacencies_table = adjacencies_table
+        self.adjacencies_table: AdjacenciesTable = adjacencies_table if adjacencies_table else dict()
 
     def add_node(self, node_label: str, adjacencies: list[AdjacencyItem]) -> None:
         self.adjacencies_table[node_label] = adjacencies
-
-    def set_pointer(self, node: str) -> None:
-        self._pointer = node
-
-    def get_pointer(self) -> str:
-        return self._pointer
 
     def get_border(self, node: str) -> list[AdjacencyItem]:
         return self.adjacencies_table[node]
