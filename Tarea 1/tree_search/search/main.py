69d748b303ff6c4125b7715c69511c22ec7d0253
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2023-08-27 15:51:23 -0600

    feat: complete tree_search algorithm
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

--- Tarea 1/tree_search/search/main.py
+++ Tarea 1/tree_search/search/main.py
@@ -1,26 +1,49 @@
 __all__ = ['tree_search']
 
-from typing import TypeVar
+from typing import Callable, TypeAlias
 from pqueue.models import PQueue
 from graph.models import LabeledGraph
 
-T = TypeVar('T')
+GoalFunction: TypeAlias = Callable[[str], bool]
+Strategy: TypeAlias = Callable[[
+    str, int, PQueue[tuple[str, int]],
+    set[str], LabeledGraph
+], None]
 
 
-def tree_search(strategy, graph: LabeledGraph, initial_node: str, goal_test):
-    border = PQueue()
-    border.push((initial_node, 0))
-    visited_nodes = set()
+def tree_search(
+    strategy: Strategy,
+    graph: LabeledGraph,
+    initial_node: str,
+    goal_test: GoalFunction
+):
+    border: PQueue[tuple[str, int]] = PQueue()
+    border.push(((initial_node, 0), 0))
+    tree: LabeledGraph = LabeledGraph()
+    visited_nodes: set[str] = set()
+    prev_node: str = ''
     while len(border):
-        graph.set_pointer(border.pop()[0])
-        visited_nodes.add(graph.get_pointer())
-        if goal_test(graph.get_pointer()):
-            return graph.get_pointer()
-        else:
-            strategy(
-                visited_nodes,
-                border,
-                graph.get_border(graph.get_pointer())
-            )
+        ((curr_node, weight), priority) = border.pop()
+        tree.add_node(
+            curr_node,
+            [(prev_node, weight)] if prev_node else []
+        )
+        visited_nodes.add(curr_node)
+        if goal_test(curr_node):
+            result: list[tuple[str, int]] = [(curr_node, 0)]
+            iter_node = curr_node
+            while len(tree.get_border(iter_node)):
+                [(iter_node, iter_weight)] = tree.get_border(iter_node)
+                result.insert(0, (iter_node, iter_weight))
 
+            return result
+        else:
+            prev_node = curr_node
+            strategy(
+                curr_node,
+                priority,
+                border,
+                visited_nodes,
+                graph,
+            )
     return None
